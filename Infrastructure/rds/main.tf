977a09a13564ccea1a292044003caf98486cdbab
Yagnash Keeka <yagnash.keeka@disraptor.co.za>
2026-02-05 11:41:02 +0200

    Updated the rds Subnet group and dev and prod

--- Infrastructure/rds/main.tf
+++ Infrastructure/rds/main.tf
@@ -17,8 +17,8 @@ resource "aws_db_instance" "legal_ascend_db" {
   engine_version = var.mysql_engine_version
   port           = 3306
 
-  instance_class = var.instance_class
-  storage_type   = var.storage_type # "gp3" recommended
+  instance_class        = var.instance_class
+  storage_type          = var.storage_type # "gp3" recommended
   allocated_storage     = var.allocated_storage
   max_allocated_storage = var.max_allocated_storage
   multi_az              = false
@@ -37,16 +37,26 @@ resource "aws_db_instance" "legal_ascend_db" {
   maintenance_window = "sun:04:00-sun:05:00"
 
   # Recommended defaults
-  storage_encrypted   = true
-  deletion_protection = true
-  skip_final_snapshot = false
+  storage_encrypted         = true
+  deletion_protection       = true
+  skip_final_snapshot       = false
   final_snapshot_identifier = "visionary-writings-${var.environment}-final"
 
   # Networking / security
-  db_subnet_group_name         = var.db_subnet_group_name
+  db_subnet_group_name         = aws_db_subnet_group.rds_private.name
   vpc_security_group_ids       = var.rds_security_group_ids
   publicly_accessible          = false
   performance_insights_enabled = var.performance_insights_enabled
 
   tags = merge(local.tags, { Name = "${local.name}-${var.environment}-db" })
+}
+
+# ----------------------------
+# RDS Subnet Group (PRIVATE ONLY)
+# ----------------------------
+resource "aws_db_subnet_group" "rds_private" {
+  name       = "${var.environment}-${local.name}-rds-private-subnets"
+  subnet_ids = var.private_subnet_ids
+
+  tags = merge(local.tags, { Name = "${local.name}-rds-subnet-group" })
 }
